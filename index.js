89f39b649450d65952636ad6ad47e3dabd70df83
Taibur Rahman <taiburrahamanapu@gmail.com>
2026-05-19 11:00:22 +0600

    fixed typo in get parcel by id api

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ const result = await  cursor.toArray();
 res.send(result)
 })
 
-app.get("/pacels/:id", async(req, res)=>{
+app.get("/parcels/:id", async(req, res)=>{
   const id = req.params.id
   const query = {_id: new ObjectId(id)}
   const result = await parcelsCollection.findOne(query)
