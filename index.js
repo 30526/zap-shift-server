8202ec7070a4d4f9fb84d571f1d9070a9f5daad1
Taibur Rahman <taiburrahamanapu@gmail.com>
2026-06-05 12:52:28 +0600

    created api for rider to accept or reject a parcel

--- index.js
+++ index.js
@@ -175,6 +175,20 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/parcels/rider", async (req, res) => {
+      const { riderEmail, deliveryStatus } = req.query;
+      const query = {};
+      if (riderEmail) {
+        query.riderEmail = riderEmail;
+      }
+      if (deliveryStatus) {
+        query.deliveryStatus = deliveryStatus;
+      }
+      const cursor = parcelsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     app.get("/parcels/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -195,7 +209,7 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
-          deliveryStatus: "picked-up",
+          deliveryStatus: "driver_assigned",
           riderId,
           riderName,
           riderEmail,
